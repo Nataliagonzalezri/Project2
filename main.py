a3f1b477dc2475b8c78a74004c398cad840922be
Natalia Gonzalez <nataliagonzalezri@gmail.com>
2021-03-04 15:56:36 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input("What is your name?")
+print("The name you entered was", name)
+greeting = "How old are you,"+name+"?"
+age = input(greeting)
+year = 2021 - int(age)
+print(name, ", you are born in:", year)
+
+
+
+
+
